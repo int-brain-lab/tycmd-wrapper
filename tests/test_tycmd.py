1f138b7cae77d250b4c4ccb9ff24e7fbab07886a
Florian Rau <bimac@users.noreply.github.com>
2024-06-30 16:04:15 +0200

    Update test_tycmd.py

--- tests/test_tycmd.py
+++ tests/test_tycmd.py
@@ -1,3 +1,5 @@
+import warnings
+
 import tycmd
 import re
 
@@ -5,6 +7,7 @@ import re
 def test_version():
     output = tycmd.version(full=True)
     assert isinstance(output, str)
+    warnings.warn(output)
     match = re.search(r"^.+(\d+\.\d+\.\d+).+$", output)
     assert match is not None
     assert match.groups()[0] == tycmd._TYCMD_VERSION
