66c834b39c5658c281b8e2738af7008d99644c1d
Florian Rau <bimac@users.noreply.github.com>
2024-06-30 21:17:25 +0200

    Update test_tycmd.py

--- tests/test_tycmd.py
+++ tests/test_tycmd.py
@@ -9,3 +9,16 @@ def test_version():
     assert match is not None
     assert match.groups()[0] == tycmd._TYCMD_VERSION
     assert tycmd.version(full=False) == tycmd._TYCMD_VERSION
+
+
+def test__assemble_tag():
+    output = tycmd._assemble_tag(serial='serial')
+    assert output == ['-B', 'serial']
+    output = tycmd._assemble_tag(family='family')
+    assert output == ['-B', '-family']
+    output = tycmd._assemble_tag(port='port')
+    assert output == ['-B', '@port']
+    output = tycmd._assemble_tag(serial='serial', family='family', port='port')
+    assert output == ['-B', 'serial-family@port']
+    output = tycmd._assemble_tag()
+    assert output == []
