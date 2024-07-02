1cff4ebfabbb6712bb8588d894809512de33db00
Florian Rau <bimac@users.noreply.github.com>
2024-07-02 12:17:02 +0200

    don't change platform tag for linux "build"

--- pdm_build.py
+++ pdm_build.py
@@ -20,22 +20,26 @@ def pdm_build_initialize(context: Context):
         case _:
             raise NotImplementedError()
     assert tycmd.exists()
-
     wheel_data = context.config.build_config.get("wheel-data", dict())
     wheel_data["scripts"] = [{"path": str(tycmd), "relative-to": str(tycmd.parent)}]
     context.config.build_config["wheel-data"] = wheel_data
 
 
 def pdm_build_finalize(context: Context, artifact: Path):
-    if context.target != "wheel":
-        return
+    pattern = r"(^[^-]+)-([^-]+)-([^-]+)-([^-]+)-([^-\.]+)"
+    name_tag, version_tag, python_tag, abi_tag, platform_tag = re.match(
+        pattern, artifact.name
+    ).groups()
+    python_tag = 'py3'
+    abi_tag = 'none'
     match system():
         case "Linux" if "64bit" in architecture():
-            platform = 'linux_x86_64'
+            pass
         case "Windows" if "64bit" in architecture():
-            platform = 'win_amd64'
+            platform_tag = 'win_amd64'
         case "Darwin":
-            platform = 'macosx_10_9_x86_64'
-    pattern = r'(^\w+-[\d\.]+)-.*'
-    new_name = re.sub(pattern, fr'\1-py3-none-{platform}.whl', str(artifact.name))
+            platform_tag = 'macosx_10_9_x86_64'
+        case _:
+            raise NotImplementedError()
+    new_name = f'{name_tag}-{version_tag}-{python_tag}-{abi_tag}-{platform_tag}.whl'
     artifact.rename(artifact.with_name(new_name))
