06c17e7a56e043e13b515c11458dd2fcc2b52cbb
pdesanex <bimac@users.noreply.github.com>
2024-07-11 11:11:14 +0100

    fix CI

--- pdm_build.py
+++ pdm_build.py
@@ -5,11 +5,8 @@ from platform import system, architecture
 from pdm.backend.hooks import Context
 
 
-def pdm_build_hook_enabled(context: Context):
-    return context.target == "wheel"
-
-
 def pdm_build_initialize(context: Context):
+    wheel_data = context.config.build_config.get("wheel-data", dict())
     match system():
         case "Linux" if "64bit" in architecture():
             tycmd = Path("bin", "linux64", "tycmd")
@@ -20,16 +17,15 @@ def pdm_build_initialize(context: Context):
         case _:
             raise NotImplementedError()
     assert tycmd.exists()
-    wheel_data = context.config.build_config.get("wheel-data", dict())
     wheel_data["scripts"] = [{"path": str(tycmd), "relative-to": str(tycmd.parent)}]
     context.config.build_config["wheel-data"] = wheel_data
 
 
 def pdm_build_finalize(context: Context, artifact: Path):
+    if context.target == "sdist":
+        return
     pattern = r"(^[^-]+)-([^-]+)-([^-]+)-([^-]+)-([^-\.]+)"
-    name_tag, version_tag, python_tag, abi_tag, platform_tag = re.match(
-        pattern, artifact.name
-    ).groups()
+    name_tag, version_tag, python_tag, abi_tag, platform_tag = re.match(pattern, artifact.name).groups()
     python_tag = 'py3'
     abi_tag = 'none'
     match system():
