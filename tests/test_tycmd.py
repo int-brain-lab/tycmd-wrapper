082aa7f29e56def54d2ed13fcbe79d6f976db8b3
pdesanex <bimac@users.noreply.github.com>
2024-07-11 15:56:24 +0100

    add identify()

--- tests/test_tycmd.py
+++ tests/test_tycmd.py
@@ -1,5 +1,22 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
+
 import tycmd
 import re
+
+
+BLINK_HEX = Path(__file__).parent.joinpath("blink.hex").resolve()
+
+
+def test_identify():
+    with TemporaryDirectory() as temp_directory:
+        firmware_file = Path(temp_directory).joinpath("firmware.hex")
+        firmware_file.touch()
+        with pytest.raises(RuntimeError):
+            tycmd.identify(firmware_file)
+    assert "Teensy 4.0" in tycmd.identify(BLINK_HEX)
 
 
 def test_version():
@@ -9,6 +26,21 @@ def test_version():
     assert match is not None
     assert match.groups()[0] == tycmd._TYCMD_VERSION
     assert tycmd.version(full=False) == tycmd._TYCMD_VERSION
+
+
+def test__parse_firmware_file():
+    with TemporaryDirectory() as temp_directory:
+        with pytest.raises(IsADirectoryError):
+            tycmd._parse_firmware_file(temp_directory)
+        firmware_file = Path(temp_directory).joinpath("firmware")
+        with pytest.raises(FileNotFoundError):
+            tycmd._parse_firmware_file(firmware_file)
+        firmware_file.touch()
+        with pytest.raises(ValueError):
+            tycmd._parse_firmware_file(firmware_file)
+        firmware_file = firmware_file.with_suffix(".hex")
+        firmware_file.touch()
+        assert tycmd._parse_firmware_file(str(firmware_file)) == firmware_file
 
 
 def test__assemble_tag():
